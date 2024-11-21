9bc6d9f404742fab79711c75101f4baeeff509eb
sanyochek1 <tyryskinsasa4@gmail.com>
2024-11-21 23:24:15 +0300

    lab ok

---
+++ homework3_lab/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("№1");
+        /*Написать программу, которая читает с экрана число от 1 до 365 (номер дня
+в году), переводит этот число в месяц и день месяца. Например, число 40 соответствует 9
+февраля (високосный год не учитывать).*/
+
+
+
+        Console.Write("Введите номер дня в году (1-365): ");
+        try
+        {
+            int dayOfYear = int.Parse(Console.ReadLine());
+
+            if (dayOfYear < 1 || dayOfYear > 365)
+            {
+                Console.WriteLine("Ошибка: номер дня должен быть в диапазоне от 1 до 365.");
+                return;
+            }
+
+            int[] daysInMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            int month = 0;
+
+            for (int i = 0; i < daysInMonth.Length; i++)
+            {
+                if (dayOfYear <= daysInMonth[i])
+                {
+                    month = i + 1;
+                    break;
+                }
+                dayOfYear -= daysInMonth[i];
+            }
+
+            Console.WriteLine($"День {dayOfYear} соответствует {month} месяцу.");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Ошибка: ввод должен быть числом.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+
+
+
+            Console.WriteLine("№2");
+            /*Добавить к задаче из предыдущего упражнения проверку числа введенного
+пользователем. Если число меньше 1 или больше 365, программа должна вырабатывать
+исключение, и выдавать на экран сообщение.*/
+
+
+            Console.Write("Введите номер дня в году (1-365): ");
+            try
+            {
+                int dayOfYear = int.Parse(Console.ReadLine());
+
+                if (dayOfYear < 1 || dayOfYear > 365)
+                {
+                    throw new ArgumentOutOfRangeException("Номер дня должен быть в диапазоне от 1 до 365.");
+                }
+
+                int[] daysInMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+                int month = 0;
+
+                for (int i = 0; i < daysInMonth.Length; i++)
+                {
+                    if (dayOfYear <= daysInMonth[i])
+                    {
+                        month = i + 1;
+                        break;
+                    }
+                    dayOfYear -= daysInMonth[i];
+                }
+
+                Console.WriteLine($"День {dayOfYear} соответствует {month} месяцу.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: ввод должен быть числом.");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Программа завершила свою работу.");
+            }
+        }
+        }
+    }
---
+++ homework3_lab/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("homework3_lab")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("homework3_lab")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("49144688-5fd9-44a5-8bf7-714ca5201d45")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
